17a6a429fde22204e651fade47702d05d9a4f8fe
Ryan Ranson <ryan.ranson@gmail.com>
2017-03-12 20:58:19 -0500

    Committing modification from last year

--- R/DownloadProjections.R
+++ R/DownloadProjections.R
@@ -1,14 +1,10 @@
-# Many thanks to fantasyfootballanalytics.net
 
-#Load libraries
-library(XML)
 
-#Download fantasy football projections from FantasyPros.com
-qb.projected <- readHTMLTable("http://www.fantasypros.com/nfl/projections/qb.php", stringsAsFactors = FALSE)$data
-rb.projected <- readHTMLTable("http://www.fantasypros.com/nfl/projections/rb.php", stringsAsFactors = FALSE)$data
-wr.projected <- readHTMLTable("http://www.fantasypros.com/nfl/projections/wr.php", stringsAsFactors = FALSE)$data
-te.projected <- readHTMLTable("http://www.fantasypros.com/nfl/projections/te.php", stringsAsFactors = FALSE)$data
-k.projected <- readHTMLTable("http://www.fantasypros.com/nfl/projections/k.php", stringsAsFactors = FALSE)$data
 
-save(qb.projected, rb.projected, wr.projected, te.projected, k.projected, 
-     file = paste0(getwd(), "/GitHub/FantasyFootball/Data/Projections-", Sys.Date(), ".RData"))
+dk.salaries$Proj[match(qb.projected$Player, dk.salaries$Name)] <- qb.projected$FPTS
+
+df <- merge(dk.salaries, qb.projected[, c("Player", "FPTS")], by.x = "Name", by.y = "Player", all.x = TRUE)
+df <- merge(df, rb.projected[, c("Player", "FPTS")], by.x = "Name", by.y = "Player", all.x = TRUE)
+df <- merge(df, wr.projected[, c("Player", "FPTS")], by.x = "Name", by.y = "Player", all.x = TRUE)
+df <- merge(df, te.projected[, c("Player", "FPTS")], by.x = "Name", by.y = "Player", all.x = TRUE)
+df <- merge(df, k.projected[, c("Player", "FPTS")], by.x = "Name", by.y = "Player", all.x = TRUE)
